f4653b0ee47ade38cb58d64694f95ce3fd11fb98
Ulas Tuerkmen <utuerkmen@newstore.com>
2016-02-21 11:09:16 +0100

    One more unittest

--- tests/unit/test_process_line.py
+++ tests/unit/test_process_line.py
@@ -2,10 +2,17 @@ import unittest
 
 from stashpy import LineProcessor
 
+SPEC = {'to_dict':["My name is {name} and I'm {age:d} years old."]}
+
 class LineProcessorTests(unittest.TestCase):
 
     def test_to_dict(self):
-        spec = {'to_dict':["My name is {name} and I'm {age:d} years old."]}
-        processor = LineProcessor(spec)
+        processor = LineProcessor(SPEC)
         dicted = processor("My name is Valerian and I'm 3 years old.")
         self.assertDictEqual(dicted, {'name': 'Valerian', 'age': 3})
+
+
+    def test_none_on_no_match(self):
+        processor = LineProcessor(SPEC)
+        dicted = processor("I'm not talking to you")
+        self.assertIsNone(dicted)
